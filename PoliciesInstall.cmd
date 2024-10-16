585e413d09263df9d669ba3eea153e50475b9d75
Gorstak45 <znastidobrostoje@gmail.com>
2024-10-16 08:07:35 +0100

    Add files via upload

---
+++ PoliciesInstall.cmd
@@ -0,0 +1,11 @@
+@echo off
+
+:: Elevate
+>nul 2>&1 fsutil dirty query %systemdrive% || echo CreateObject^("Shell.Application"^).ShellExecute "%~0", "ELEVATED", "", "runas", 1 > "%temp%\uac.vbs" && "%temp%\uac.vbs" && exit /b
+DEL /F /Q "%temp%\uac.vbs"
+
+:: Move to the script directory
+cd /d %~dp0
+
+:: Install Policies
+lgpo /g ./
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Policies
+windows security baseline

68658ef9d5e32720c10aa77bff6d198d0b7426f2
Gorstak45 <znastidobrostoje@gmail.com>
2024-10-19 11:52:12 +0200

    Add files via upload

---
+++ SetupComplete.cmd
@@ -0,0 +1,14 @@
+@echo off
+
+:: Elevate
+>nul 2>&1 fsutil dirty query %systemdrive% || echo CreateObject^("Shell.Application"^).ShellExecute "%~0", "ELEVATED", "", "runas", 1 > "%temp%\uac.vbs" && "%temp%\uac.vbs" && exit /b
+DEL /F /Q "%temp%\uac.vbs"
+
+:: Move to the script directory
+cd /d %~dp0
+
+:: Move to Bin subfolder
+cd Bin
+
+:: Policy
+lgpo /g ./
